207577432d31712b7c718c08114bf9a54b8b68dd
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2022-11-02 17:07:42 -0400

    mongo db modules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
