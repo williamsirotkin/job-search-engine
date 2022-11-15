6654d906cec31734fa9f4df462cd7ce6bba89139
Timothy Hall <timdhall33@gmail.com>
2022-11-14 22:04:20 -0500

    nav bar updated

--- app/src/NavBar/NavBar.js
+++ app/src/NavBar/NavBar.js
@@ -38,8 +38,7 @@ function Login() {
 
     return (
         <div class = "navBar">
-            <img src = {require('../images/logo.png')} alt = "logo"/>>
-              <h1 class ="title"> Job Search Engine </h1>
+            <img src = {require('../images/logo.png')} alt = "logo" class = "logo"/>
             <div class = "login">
                 {profile ? (
                     <div>
