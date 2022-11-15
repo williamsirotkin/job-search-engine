e6ab54306df46feb42396ac3770e15804df9bcf6
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2022-11-14 22:03:26 -0500

    update

--- app/src/NavBar/NavBar.js
+++ app/src/NavBar/NavBar.js
@@ -38,7 +38,7 @@ function Login() {
 
     return (
         <div class = "navBar">
-            <img src = {require('../images/logo.png')} alt = "logo"/>>
+            <img src = {require('../images/logo.png')} alt = "logo"/>
               <h1 class ="title"> Job Search Engine </h1>
             <div class = "login">
                 {profile ? (
